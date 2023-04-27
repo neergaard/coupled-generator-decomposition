9aa8e30a48bfaa1c7c236be9422e31c353f15d61
Anders <ansol@dtu.dk>
2023-04-27 10:00:52 +0200

    fix

--- submitfiles/run_LARS.sh
+++ submitfiles/run_LARS.sh
@@ -3,6 +3,7 @@ do
 for M in 0 1 2
 do
 for O in 0 1 2 3 4
+do
 
     sed -i '$ d' submit_LARS.sh
     echo "python3 experiments/run_SPCA_QP.py $M $K $O" >> submit_LARS.sh
--- submitfiles/run_SPCA.sh
+++ submitfiles/run_SPCA.sh
@@ -3,6 +3,7 @@ do
 for M in 0 1 2
 do
 for O in 0 1 2 3 4
+do
 
     sed -i '$ d' submit_SPCA.sh
     echo "python3 experiments/run_SPCA.py $M $K $O" >> submit_SPCA.sh
