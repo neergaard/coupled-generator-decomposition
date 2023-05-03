114e2f6e18278ef943f4f812c08e58483e7c6716
Anders <ansol@dtu.dk>
2023-05-03 08:35:57 +0200

    fix

---
+++ submitfiles/run_SPCA_selectedregu.sh
@@ -0,0 +1,11 @@
+for M1 in 0 1
+do
+for M2 in 0 1
+do
+
+    sed -i '$ d' submit_SPCA_selectedregu.sh
+    echo "python3 experiments/submit_SPCA_selectedregu.py $M1 $M2" >> submit_SPCA_selectedregu.sh
+    bsub < submit_SPCA_selectedregu.sh
+
+done
+done
